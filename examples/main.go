552f4ba90a3d67951dc198a34c3a639204dd2ca4
Maxim Levchenko <kagbe.leviy@gmail.com>
2026-04-21 19:52:20 +0300

    fix: handle parser help errors via *flags.Error with errors.As

--- examples/main.go
+++ examples/main.go
@@ -82,14 +82,10 @@ var parser = flags.NewParser(&options, flags.Default)
 
 func main() {
 	if _, err := parser.Parse(); err != nil {
-		switch flagsErr := err.(type) {
-		case flags.ErrorType:
-			if flagsErr == flags.ErrHelp {
-				os.Exit(0)
-			}
-			os.Exit(1)
-		default:
-			os.Exit(1)
+		var flagsErr *flags.Error
+		if errors.As(err, &flagsErr) && flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
 		}
+		os.Exit(1)
 	}
 }
