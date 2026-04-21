a1f1f74709017b7dbc42528db547c6133c6beb1a
Maxim Levchenko <kagbe.leviy@gmail.com>
2026-04-21 19:30:37 +0300

    chore: update files header to SPDX

--- group.go
+++ group.go
@@ -1,6 +1,7 @@
-// Copyright 2012 Jesse van den Kieboom. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// SPDX-FileType: SOURCE
+// SPDX-FileCopyrightText: 2012 Jesse van den Kieboom
+// SPDX-FileCopyrightText: 2026 Maxim Levchenko (WoozyMasta)
+// SPDX-License-Identifier: BSD-3-Clause
 
 package flags
 
