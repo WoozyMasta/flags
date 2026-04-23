7be6778182a64809c7dfb3fea458fc05fac50855
Maxim Levchenko <kagbe.leviy@gmail.com>
2026-04-24 02:39:49 +0300

    chore: normalize file headers to SPDX + project reference

--- flags.go
+++ flags.go
@@ -1,7 +1,6 @@
 // SPDX-FileType: SOURCE
-// SPDX-FileCopyrightText: 2012 Jesse van den Kieboom
-// SPDX-FileCopyrightText: 2026 Maxim Levchenko (WoozyMasta)
 // SPDX-License-Identifier: BSD-3-Clause
+// Project: https://github.com/woozymasta/flags
 
 /*
 Package flags provides a reflection-based command-line parser.
